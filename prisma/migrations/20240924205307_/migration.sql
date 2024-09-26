7b3e7533c0ecd48717ae739fc85f9a5bb9cd62c4
ayhankos <ayhanx926@gmail.com>
2024-09-26 22:15:56 +0300

    .

--- prisma/migrations/20240924205307_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `price` on the `Server` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Server` DROP COLUMN `price`,
-    ADD COLUMN `Rank` VARCHAR(191) NOT NULL DEFAULT '0';
