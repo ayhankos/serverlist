cd15465d0befe55b8f9adf56b63487e957a5bf64
ayhankos <ayhanx926@gmail.com>
2024-09-27 01:57:44 +0300

    .

--- app/api/uploadImage/route.ts
+++ app/api/uploadImage/route.ts
@@ -1,41 +1,31 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import path from "path";
+import { writeFile } from "fs/promises";
 
-const CWP_UPLOAD_URL = process.env.NEXT_PUBLIC_UPLOAD_URL as string;
-
-export const POST = async (req: NextRequest) => {
+export const POST = async (req: any, res: any) => {
   const formData = await req.formData();
 
-  const files = formData.getAll("files") as File[];
-
+  const files = formData.getAll("files");
   if (files.length === 0) {
     return NextResponse.json({ error: "No files received." }, { status: 400 });
   }
 
   const responses = [];
-
   for (const file of files) {
-    const newFormData = new FormData();
-    newFormData.append("file", file);
-
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const filename = `${new Date().getTime()}${Math.random()
+      .toString(36)
+      .substring(7)}.${file.type.split("/")[1]}`;
+    console.log(filename);
     try {
-      const response = await fetch(CWP_UPLOAD_URL, {
-        method: "POST",
-        body: newFormData,
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const result = await response.json();
-      responses.push(result);
+      await writeFile(
+        path.join(process.cwd(), "public/uploads/" + filename),
+        buffer
+      );
+      responses.push({ file: filename, status: "Success" });
     } catch (error) {
-      console.error("Error uploading file:", error);
-      responses.push({
-        status: "error",
-        message: "Failed to upload file",
-        file: file.name,
-      });
+      console.log("Error occurred ", error);
+      responses.push({ file: filename, status: "Failed" });
     }
   }
 
