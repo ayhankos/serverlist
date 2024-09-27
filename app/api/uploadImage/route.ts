87c5b78fdec70f940f712fc7a208e9bad3dfa3f6
ayhankos <ayhanx926@gmail.com>
2024-09-27 10:29:42 +0300

    apı deneme

--- app/api/uploadImage/route.ts
+++ app/api/uploadImage/route.ts
@@ -1,36 +1,52 @@
 import { NextResponse } from "next/server";
-import path from "path";
-import { writeFile } from "fs/promises";
 
-export const POST = async (req: any, res: any) => {
+export const POST = async (req: any) => {
   const formData = await req.formData();
-
   const files = formData.getAll("files");
+
   if (files.length === 0) {
     return NextResponse.json({ error: "No files received." }, { status: 400 });
   }
 
-  const responses = [];
+  const formDataToSend = new FormData();
+
   for (const file of files) {
     const buffer = Buffer.from(await file.arrayBuffer());
     const filename = `${new Date().getTime()}${Math.random()
       .toString(36)
       .substring(7)}.${file.type.split("/")[1]}`;
-    console.log(filename);
-    try {
-      await writeFile(
-        path.join(process.cwd(), "public/uploads/" + filename),
-        buffer
-      );
-      responses.push({ file: filename, status: "Success" });
-    } catch (error) {
-      console.log("Error occurred ", error);
-      responses.push({ file: filename, status: "Failed" });
-    }
+
+    formDataToSend.append("file", new Blob([buffer]), filename);
   }
 
-  return NextResponse.json(
-    { Message: "Processed", responses },
-    { status: 201 }
-  );
+  try {
+    const response = await fetch("https://pvpserverlar.tr/upload.php", {
+      method: "POST",
+      body: formDataToSend,
+      headers: {
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      return NextResponse.json(
+        { message: "Files uploaded successfully", result },
+        { status: 201 }
+      );
+    } else {
+      return NextResponse.json(
+        { error: result.message || "Upload failed" },
+        { status: response.status }
+      );
+    }
+  } catch (error) {
+    console.log("Error occurred while sending files to the PHP server:", error);
+    return NextResponse.json(
+      { error: "Error occurred while sending files" },
+      { status: 500 }
+    );
+  }
 };
