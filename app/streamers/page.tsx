cc40f986ce65107a8950fde9c1639d3aaa60f97b
ayhankos <ayhanx926@gmail.com>
2024-09-29 18:48:42 +0300

    streamer page build error fix

--- app/streamers/page.tsx
+++ app/streamers/page.tsx
@@ -2,29 +2,29 @@ import { Metadata } from "next";
 import Link from "next/link";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import useSWR from "swr";
 import { FaYoutube, FaDiscord } from "react-icons/fa";
-import { Streamer } from "@prisma/client";
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  AwaitedReactNode,
+} from "react";
+import { UrlObject } from "url";
 
 export const metadata: Metadata = {
   title: "Streamers Dashboard",
   description: "Dashboard showcasing popular streamers.",
 };
 
-const fetcher = async (url: string | URL | Request) => {
-  const res = await fetch(url);
-  if (!res.ok) throw new Error("Failed to fetch");
-  return res.json();
-};
+export default async function StreamersPage() {
+  const response = await fetch("/api/adminStreamers");
 
-export default function StreamersPage() {
-  const { data: streamers, error } = useSWR<Streamer[]>(
-    "/api/streamers",
-    fetcher
-  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch streamers");
+  }
 
-  if (error) return <div>Error loading streamers: {error.message}</div>;
-  if (!streamers) return <div>Loading...</div>;
+  const streamers = await response.json();
 
   return (
     <div className="hidden md:block container">
@@ -40,60 +40,77 @@ export default function StreamersPage() {
           </div>
         </div>
         <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10">
-          {streamers.map((streamer) => (
-            <Card key={streamer.id} className="bg-white">
-              <CardContent className="p-4">
-                <div className="aspect-square relative mb-2">
-                  <img
-                    src={streamer.image}
-                    alt={streamer.name}
-                    className="w-full h-full object-cover rounded-full"
-                  />
-                </div>
-                <h3 className="font-semibold text-center mb-2">
-                  {streamer.name}
-                </h3>
-                <div className="flex justify-center space-x-2">
-                  {streamer.ytLink && (
-                    <Link
-                      href={streamer.ytLink}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Button
-                        size="sm"
-                        className="bg-white hover:bg-zinc-200"
-                        variant="outline"
+          {streamers?.map(
+            (streamer: {
+              id: Key | null | undefined;
+              image: string | undefined;
+              name:
+                | string
+                | number
+                | bigint
+                | boolean
+                | ReactElement<any, string | JSXElementConstructor<any>>
+                | Iterable<ReactNode>
+                | Promise<AwaitedReactNode>
+                | null
+                | undefined;
+              ytLink: string | UrlObject;
+              dcLink: string | UrlObject;
+            }) => (
+              <Card key={streamer.id} className="bg-white">
+                <CardContent className="p-4">
+                  <div className="aspect-square relative mb-2">
+                    <img
+                      src={streamer.image}
+                      alt={streamer.name?.toString()}
+                      className="w-full h-full object-cover rounded-full"
+                    />
+                  </div>
+                  <h3 className="font-semibold text-center mb-2">
+                    {streamer.name}
+                  </h3>
+                  <div className="flex justify-center space-x-2">
+                    {streamer.ytLink && (
+                      <Link
+                        href={streamer.ytLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
                       >
-                        <FaYoutube
-                          className="h-8 w-8"
-                          style={{ color: "#FF0000" }}
-                        />
-                      </Button>
-                    </Link>
-                  )}
-                  {streamer.dcLink && (
-                    <Link
-                      href={streamer.dcLink}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Button
-                        size="sm"
-                        className="bg-white hover:bg-zinc-200"
-                        variant="outline"
+                        <Button
+                          size="sm"
+                          className="bg-white hover:bg-zinc-200"
+                          variant="outline"
+                        >
+                          <FaYoutube
+                            className="h-8 w-8"
+                            style={{ color: "#FF0000" }}
+                          />
+                        </Button>
+                      </Link>
+                    )}
+                    {streamer.dcLink && (
+                      <Link
+                        href={streamer.dcLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
                       >
-                        <FaDiscord
-                          className="h-8 w-8"
-                          style={{ color: "#7289DA" }}
-                        />
-                      </Button>
-                    </Link>
-                  )}
-                </div>
-              </CardContent>
-            </Card>
-          ))}
+                        <Button
+                          size="sm"
+                          className="bg-white hover:bg-zinc-200"
+                          variant="outline"
+                        >
+                          <FaDiscord
+                            className="h-8 w-8"
+                            style={{ color: "#7289DA" }}
+                          />
+                        </Button>
+                      </Link>
+                    )}
+                  </div>
+                </CardContent>
+              </Card>
+            )
+          )}
         </div>
       </div>
     </div>
