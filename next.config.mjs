a8b849613eec3d520aa9ebd7a6e66bfe4c0baa8a
ayhankos <ayhanx926@gmail.com>
2024-10-04 01:34:20 +0300

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["pvpserverlar.tr"],
+    unoptimized: true,
+  },
+  output: "standalone",
+};
+
+module.exports = nextConfig;
 
 export default nextConfig;
